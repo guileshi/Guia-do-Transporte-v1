dc350ba9dad0ea3d1b7e07421c8808dccf6d0588
guileshi <btguile@hotmail.com>
2022-01-06 10:23:46 -0300

    layout dos comentários da página das transportadoras / outros pequenos ajustes

--- js/rating.js
+++ js/rating.js
@@ -1,43 +1,13 @@
-const rating = [...document.querySelector('#rating').children];
-
 const stars = [...document.querySelector('#stars').children];
 
-var starsAvegare;
 
-function getAverage(){
-    starRating(starsAvegare - 1);
-}
-
-function starRating(star){
-
-    return rating.map((item, index) => {
-        if (index <= star) {
-            item.classList.add('star__active');
-            return;
-        }
-        
-        item.classList.remove('star__active');
-    });
-
-}
-
-function clickStar(star){
-
+  function clickStar(starsIndex) {
     stars.map((item, index) => {
-        if (index <= star) {
-            item.classList.add('star__active');
-            return;
-        }
-        
-        item.classList.remove('star__active');
+      if (index <= starsIndex) {
+        item.classList.add('star__active');
+        return;
+      }
+
+      item.classList.remove('star__active');
     });
-
-    starsAvegare = star + 1 
-}
-
-
-$(window).load(() => {
-
-    getAverage()
-
-})
+  }
