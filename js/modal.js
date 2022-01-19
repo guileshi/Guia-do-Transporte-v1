0ee03cd4a17c1e5c4f9c5275bdadeb2915413279
guileshi <btguile@hotmail.com>
2022-01-19 17:06:38 -0300

    new quotation/rotes page

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 function toggleModal(modal, type, input, inputVal, typeOfInput){
     
-    if(document.getElementById(modal).id == 'switchPicture__modal'){
+    if(document.getElementById(modal).id != 'switchMyData__modal'){
         $(`#${modal}`).toggleClass('hide')
 
     }else{
@@ -17,6 +17,71 @@ function toggleModal(modal, type, input, inputVal, typeOfInput){
         } else{
             $(`#${modal}`).toggleClass('hide')    
         }
+    }
+
+}
+
+function switchRotes(obj){
+    $(obj).toggleClass('disabled')
+}
+
+function tutorial(){
+    new nextModal()
+}
+
+function nextModal(modal){
+
+    var helpText = document.getElementById('howWorks__text')
+    var helpButton = document.getElementById('howWorks__button')
+    var addButton = document.getElementById('howWorks__addButton')
+    var addButton2 = document.getElementById('howWorks__addButton2')
+
+
+    if($('#howWorks__modal').hasClass("tutorial1")){
+        helpText.innerHTML = "Ao clicar em um Estado, você verá todos os municíos disponíveis do mesmo. Selecionando um município fará com que ele fique disponível. Você também pode desmarcar um município específico se desejar."
+        helpButton.innerHTML = "Proximo (2/3)"
+        addButton.innerText = "SÃO PAULO"
+        addButton2.innerText = "GUARULHOS"
+
+        $('#howWorks__modal').removeClass("tutorial1")
+        $('#howWorks__modal').addClass("tutorial2")
+
+    } else if($('#howWorks__modal').hasClass("tutorial2")){
+        helpText.innerHTML = "Você pode clicar em <strong>ADICIONAR TODOS</strong> para adicionar todas as rotas, tanto para os estados quanto para os municípios."
+        helpButton.innerHTML = "Entendido"
+        addButton.innerHTML = "ADICIONAR TODOS..."
+
+        $('#howWorks__addAllRotes_text').removeClass("col-lg-4")
+        $('#howWorks__addAllRotes_text').removeClass("col-md-5")
+        $('#howWorks__addAllRotes_text').removeClass("col-sm-12")
+        $('#howWorks__addAllRotes_text').addClass("col-auto")
+
+        $('.howWorks__field').css("display", "none")
+
+        $('#howWorks__modal').removeClass("tutorial2")
+        $('#howWorks__modal').addClass("tutorial3")
+
+        $('#howWorks__doubt__text').removeClass('hide')
+
+    } else if($('#howWorks__modal').hasClass("tutorial3")){
+        $(`#${modal}`).toggleClass('hide')
+
+        $('.howWorks__field').css("display", "block")
+
+        $('#howWorks__addAllRotes_text').addClass("col-lg-4")
+        $('#howWorks__addAllRotes_text').addClass("col-md-5")
+        $('#howWorks__addAllRotes_text').addClass("col-sm-12")
+        $('#howWorks__addAllRotes_text').removeClass("col-auto")
+
+        helpText.innerHTML = "Ao selecionar um estado você verá todos os municípios disponíveis para escolher. Ao adicionar rotas em algum estado, ele ficará com o fundo azul e as rotas ficarão disponíveis."
+        helpButton.innerHTML = "Proximo (1/3)"
+        addButton2.innerHTML = "MG"
+        addButton.innerHTML = "SP"
+
+        $('#howWorks__doubt__text').addClass('hide')
+
+        $('#howWorks__modal').removeClass("tutorial3")
+        $('#howWorks__modal').addClass("tutorial1")
     }
 
 }
